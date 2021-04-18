6ec490b1cc93c259b1f2db081e1719a44c9b9f7e
pillar <ymblender@gamil.com>
2021-04-18 11:48:30 +0800

    save

--- lib/ux/UxInputOut.tsx
+++ lib/ux/UxInputOut.tsx
@@ -1,5 +1,5 @@
 const sty =
-  "b:1px|solid|--line-deep outline:none h:46px bg:--bg radius:--r pl:10px :focus:bg:--gray :focus-within:s:0|0|0|3px|--active t:all|0.2s|--ease box-sizing:border-box time:0.2s|all|--ease";
+  "b:1px|solid|--line-deep outline:none h:46px bg:--bg radius:--r p:0px|14px :focus:bg:--gray :focus-within:s:0|0|0|3px|--active t:all|0.2s|--ease box-sizing:border-box time:0.2s|all|--ease";
 
 export function UxOutInput({ className, ...rest }: IProps) {
   return <input class={[sty, className]} {...rest}></input>;
